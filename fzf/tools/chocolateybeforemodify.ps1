7c1915028f9aaccc847a9290920a770428eef4d3
Nik Hildebrandt <nik.hildebrandt@proton.me>
2026-02-22 20:37:36 +0100

    Added Icon and removed comments

---
+++ fzf/tools/chocolateybeforemodify.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+$processName = 'fzf'
+
+Write-Host "Checking for running '$processName' processes..."
+$processes = Get-Process -Name $processName -ErrorAction SilentlyContinue
+
+if ($processes) {
+    Write-Host "Found running '$processName' processes. Terminating them to release file locks..."
+    $processes | Stop-Process -Force -ErrorAction SilentlyContinue
+    
+    Start-Sleep -Seconds 2 
+} else {
+    Write-Host "No running '$processName' processes found."
+}
