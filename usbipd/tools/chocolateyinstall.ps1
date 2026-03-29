95c427f2422978b8b3da6433a0844ff7e11ec391
Nik Hildebrandt <nik.hildebrandt@proton.me>
2026-03-29 20:20:59 +0200

    Update script

---
+++ usbipd/tools/chocolateyinstall.ps1
@@ -0,0 +1,27 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64         = 'changeme'
+$checksum64    = 'changeme'
+$urlArm64      = 'changeme'
+$checksumArm64 = 'changeme'
+
+if ($env:PROCESSOR_ARCHITECTURE -match 'ARM') {
+    $url      = $urlArm64
+    $checksum = $checksumArm64
+} else {
+    $url      = $url64
+    $checksum = $checksum64
+}
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'MSI'
+  url64bit      = $url
+  softwareName  = 'usbipd*'
+  checksum64    = $checksum
+  checksumType64= 'sha256'
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
